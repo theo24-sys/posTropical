575d9367b266ae033557f76262356bc57a5b655c
theo24-sys <timothybanjo42@gmail.com>
2026-09-07 23:54:44 +0300

    Update base URL in kraEtims.js

--- services/kraEtims.js
+++ services/kraEtims.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DEFAULT_BASE_URL = "https://etims-api-sbx.kra.go.ke/etims-api";
+const DEFAULT_BASE_URL = "https://etims-api-sbx.kra.go.ke";
 
 function responsePreview(value) {
   try {
